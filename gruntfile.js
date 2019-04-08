2333fdb219bc7c16bfdceff2f36a8f32279da4d7
Fabio <fabio@treexor.com>
2019-04-08 16:33:52 +0100

    grunt test 14 - no deploy

--- gruntfile.js
+++ gruntfile.js
@@ -3,55 +3,57 @@ module.exports = grunt => {
     grunt.initConfig({
         shell: {
             theme_lint: {
-                command: shop => `./node_modules/.bin/theme-lint shops/${shop}/`,
+                command: shop => `./node_modules/.bin/theme-lint shops/${shop}/` //try to do this with a npm script
             },
+            git_commits_difference: {
+                //command: 'git diff HEAD^ HEAD --name-only'
+                command: 'ls'
+            }
         },
     })
     grunt.registerTask('default', ['shell'])
-    grunt.registerTask('setShopsConfig', function() {
-        var shops = grunt.file.readYAML('config.yml')
-        var configArray = []
-        var configString = ''
+    grunt.registerTask('setShopsConfig', function() { //try to reduce all the loops
+        var shops = grunt.file.readYAML('config.yml');
+        var configArray = [];
+        var configString = '';
         for (let shop in shops) {
+            
             for (let env in shops[shop]) {
                 configString += env
 
                 for (let attr in shops[shop][env]) {
                     if (attr == 'ignore_files') {
-                        configString += '\n  ' + attr + ':'
+                        configString += '\n  ' + attr + ':';
 
                         for (let file in shops[shop][env][attr]) {
                             if (shops[shop][env][attr][file].startsWith('*')) {
-                                configString +=
-                                    ' \n    - "' +
-                                    shops[shop][env][attr][file] +
-                                    '"'
+                                configString += ' \n    - "' + shops[shop][env][attr][file] + '"';
                             } else {
-                                configString +=
-                                    ' \n    - ' + shops[shop][env][attr][file]
+                                configString += ' \n    - ' + shops[shop][env][attr][file];
                             }
                         }
                     } else {
-                        configString +=
-                            '\n  ' + attr + ': ' + shops[shop][env][attr]
+                        configString += '\n  ' + attr + ': ' + shops[shop][env][attr];
                     }
                 }
 
-                configString += '\n'
+                configString += '\n';
             }
 
-            configArray[shop] = configString
-            console.log("file created");
-            grunt.file.write(`shops/${Object.keys(configArray)[0]}/config.yml`,configArray[shop])
+            configArray[shop] = configString;
+            grunt.file.write(`shops/${Object.keys(configArray)[0]}/config.yml`,configArray[shop]);
 
-            configArray = []
-            configString = ''
+            configArray = [];
+            configString = '';
         }
     })
     grunt.registerTask('theme-lint', function() {
-        var shops = grunt.file.readYAML('config.yml')
+        var shops = grunt.file.readYAML('config.yml');
         for (let shop in shops) {
-            grunt.task.run('shell:theme_lint:' + shop)
+            grunt.task.run('shell:theme_lint:' + shop);
         }
+    })
+    grunt.registerTask('getFilesToUpload', function() {
+        grunt.task.run('shell:git_commits_difference');
     })
 }
